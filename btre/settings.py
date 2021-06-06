f2a0fd766f7bdc5b8f67f1f616f64b747f093b44
Amit Kumar <amitkr0819@gmail.com>
2021-06-06 13:23:58 +0530

    made listings page dynamic and made pagination functional

--- btre/settings.py
+++ btre/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'pages',
     'listings',
     'realtors',
@@ -130,6 +131,8 @@ STATICFILES_DIRS = [
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+# Media files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
