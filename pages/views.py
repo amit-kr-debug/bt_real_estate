62f265b72815db361cbcdb48205b86145e46ec53
Amit Kumar <amitkr0819@gmail.com>
2021-06-06 15:43:02 +0530

    made index and about pages to load dynamic data

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from listings.models import Listing
+from realtors.models import Realtor
+
 
 def index(request):
-    return render(request, 'pages/index.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published = True)[:3]
+
+    context = {
+        'listings': listings
+    }
+    return render(request, 'pages/index.html', context)
 
 
 def about(request):
-    return render(request, 'pages/about.html')
+    realtors = Realtor.objects.all()
+    realtor_of_month = Realtor.objects.filter(is_mvp = True)[0]
+    context = {
+        'realtors': realtors,
+        'mvp': realtor_of_month
+    }
+    return render(request, 'pages/about.html', context)
