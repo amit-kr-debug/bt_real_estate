8614c0e6fb044eec1b39fcff24d3ef23add4ae31
Amit Kumar <amitkr0819@gmail.com>
2021-06-06 17:52:24 +0530

    made search feature functional

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+
 from .models import Listing
+from .choices import bedroom_choices, state_choices, price_choices
 
 
 def index(request):
@@ -9,14 +11,55 @@ def index(request):
     page = request.GET.get('page')
     paged_listings = paginator.get_page(page)
     content = {
-        'listings': paged_listings
+        'listings': paged_listings,
     }
     return render(request, 'listings/listings.html', content)
 
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    listing = get_object_or_404(Listing, pk=listing_id)
+    context = {
+        'listing': listing
+    }
+    return render(request, 'listings/listing.html', context)
 
 
 def search(request):
-    return render(request, 'listings/search.html')
+    query_list = Listing.objects.order_by('-list_date')
+
+    # Keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            query_list = query_list.filter(description__icontains = keywords)
+
+    # City
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            query_list = query_list.filter(city__iexact = city)
+
+    # State
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            query_list = query_list.filter(state__iexact = state)
+
+    # Bedrooms
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            query_list = query_list.filter(bedrooms__lte = bedrooms)
+
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            query_list = query_list.filter(price__lte = price)
+
+    context = {
+        'bedroom_choices': bedroom_choices,
+        'state_choices': state_choices,
+        'price_choices': price_choices,
+        'listings': query_list,
+    }
+    return render(request, 'listings/search.html', context)
