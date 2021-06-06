330a196ce766343c7006fa73ac2fb809be9dc2d1
Amit Kumar <amitkr0819@gmail.com>
2021-06-06 22:55:22 +0530

    made register and login functionalities functional

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,52 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 
 
 def register(request):
     if request.method == "POST":
-        return
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        if password == password2:
+            if User.objects.filter(username = username).exists():
+                messages.error(request,'username is taken')
+                return redirect('accounts:register')
+            else:
+                if User.objects.filter(email = email).exists():
+                    messages.error(request,'The email is being used')
+                    return redirect('accounts:register')
+                else:
+                    user = User.objects.create_user(first_name = first_name,
+                                                    last_name = last_name,
+                                                    email = email,
+                                                    username = username,
+                                                    password = password)
+                    user.save()
+                    auth.login(request, user)
+
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('accounts:register')
     return render(request, 'accounts/register.html')
 
 
 def login(request):
     if request.method == "POST":
-        return
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('accounts:dashboard')
+        else:
+            messages.error(request, 'Invalid Credentials')
+            return redirect('accounts:login')
     return render(request, 'accounts/login.html')
 
 
@@ -20,6 +57,4 @@ def logout(request):
 
 
 def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
-
-
+    return render(request,'accounts/dashboard.html')
